8713c5913a2ea84adfc24ff7b4c4a6336825b94c
mlscmoulika <53378071+mlscmoulika@users.noreply.github.com>
2021-07-01 16:48:26 +0530

    Added text module to the screenshot

--- CODE/GLYPH/Archive/Sai/CODE/ScreenShotBuilder.py
+++ CODE/GLYPH/Archive/Sai/CODE/ScreenShotBuilder.py
@@ -26,15 +26,43 @@ class ScreenShot:
         draw = ImageDraw.Draw(self.preImage)
         draw.line((location0[1]*100+50, location0[0]*100+50, location1[1]*100+50, location1[0]*100+50), width = 5, fill='yellow')
 
-    
+    # edited
+    def drawStatus(self,id,filename):
+        text=""
+        fileName = f'../DATA/DDRI_STUDY_LOGS/{filename}.json'
+
+        try:
+            data = json.load(open(fileName))
+            print("File loaded successfully")
+        except:
+            print('error')
+            exit()
+
+        for event in data['events']:
+            if event["type"]=="SET_REFLECTION_CONTENT":
+                if event['id']==id:
+                    text=text+str(event['content']['status'])+"/"+str(event['content']['simType'])
+            #     print("The status is "+str(event['content']['status']))
+            #     print("Type of simulation is "+str(event['content']['simType']))
+            # print("----------------------------")
+        font = ImageFont.truetype("Roboto-Bold.ttf",size=45)
+        # font = ImageFont.load("arial.pil")
+        (x, y) = (500, 850)
+        color = 'rgb(255, 255, 255)' 
+        draw = ImageDraw.Draw(self.preImage)
+        draw.text((x, y), text, fill=color,font=font)
+
+
     def saveImage(self):
         #self.preImage.resize((200,200), Image.ANTIALIAS)
         self.preImage.save(self.destination, quality=95)
 
 
+# uncommented for testing
 # screenshot = ScreenShot(5,'test.png')
-# screenshot.drawSemaphore(0,0)
+# screenshot.drawSemaphore(100,100)
 # screenshot.drawSemaphore(0,1)
 # screenshot.drawSignal(3,3)
-
+# screenshot.drawStatus("6da6cd8c-c573-4c62-97b3-2214f5adfbae","2e07ba3c-51be-498f-815a-768f3b7cb7e1")
 # screenshot.saveImage()
+
