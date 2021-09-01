d192708739ac4aa2c4f71069bc5a870de8f7ef5b
Sai Siddartha Maram <smaram_be16@thapar.edu>
2021-09-01 14:39:49 -0700

    adding new images

--- ParallelOPM-main/CODE/GLYPH/Archive/Sai/DATA/screenshot_dict.py
+++ ParallelOPM-main/CODE/GLYPH/Archive/Sai/DATA/screenshot_dict.py
@@ -1,19 +1,17 @@
 import glob
 import os
+ans={}
+search_dir = "Screenshots"
+for folder in os.listdir(search_dir):
+    Folder = search_dir+'/'+folder
 
-def log(search_dir):
-    ans={}
-    for folder in os.listdir(search_dir):
-        Folder = search_dir+'/'+folder
+    for filename in os.listdir(Folder):
+        ans[folder]=[] 
+    files = list(filter(os.path.isfile, glob.glob(Folder+"/" + "*")))
+    files.sort(key=lambda x: os.path.getmtime(x))
+    # print(files)
+    for list1 in files:
+        list1=list1.replace("\\", "/")
+        ans[folder].append(list1)
 
-        for filename in os.listdir(Folder):
-            ans[folder]=[] 
-        files = list(filter(os.path.isfile, glob.glob(Folder+"/" + "*")))
-        files.sort(key=lambda x: os.path.getmtime(x))
-        # print(files)
-        for list1 in files:
-            list1=list1.replace("\\", "/")
-            ans[folder].append(list1)
-    return ans
-
-print(log('Screenshots'))
+print(ans)
