25506413ed84153b4b3f909342b6a5dc3a8f7825
siddu1998 <samaram@ucsc.edu>
2022-10-31 13:09:11 -0700

    adding knowledge+abstraction results

--- ParallelOPM-main/CODE/GLYPH/Archive/Sai_cosine/CODE/getBoardID.py
+++ ParallelOPM-main/CODE/GLYPH/Archive/Sai_cosine/CODE/getBoardID.py
@@ -36,3 +36,26 @@ def get_board_ids(LOGS):
 
     return bi
 
+def get_board_ids_all(LOGS):
+    # bi= Board Ids
+    bi={}
+    for filename in os.listdir(LOGS):
+        fileName = LOGS+'/'+filename
+        log=[]
+
+        try:
+            data = json.load(open(fileName))
+        except:
+            print(fileName)
+            print('[ERROR] A game play with the eneterd data does not exist!')
+            print('[INFO] Please run the program again!')
+            print("snap")
+            exit()
+        for event in data['events']:
+            if event["type"]=="BOARD_SNAPSHOT" or event["type"] in CRITICAL_EVENTS:
+                log.append(event["id"])
+                
+        bi[filename]=log
+
+    return bi
+
